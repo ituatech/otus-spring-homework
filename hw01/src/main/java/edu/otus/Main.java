e61616816d2d3db9c2917b96126d9c2dfdf2b04c
nik <bespalov-ns@mail.ru>
2018-06-28 12:55:57 +0300

    hw01

---
+++ hw01/src/main/java/edu/otus/Main.java
@@ -0,0 +1,25 @@
+package edu.otus;
+
+import edu.otus.service.TestingService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Nik Bespalov on 27/06/2018.
+ *
+ * @author Nik Bespalov.
+ */
+public class Main {
+    /*
+    Правильные ответы:
+    1 - 2
+    2 - 1,3
+    3 - 2
+    4 - 3
+    5 - 1
+     */
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        TestingService testingService = context.getBean(TestingService.class);
+        testingService.start();
+    }
+}
---
+++ hw01/src/test/java/edu/otus/QuestionsDAOTest.java
@@ -0,0 +1,33 @@
+package edu.otus;
+
+import edu.otus.dao.QuestionsDAO;
+import edu.otus.entity.Question;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by Nik Bespalov on 28/06/2018.
+ *
+ * @author Nik Bespalov.
+ */
+public class QuestionsDAOTest {
+    ClassPathXmlApplicationContext context;
+
+    @Before
+    public void init() {
+        context = new ClassPathXmlApplicationContext("/spring-context.xml");
+    }
+
+    @Test
+    public void test() {
+        QuestionsDAO dao = context.getBean(QuestionsDAO.class);
+        List<Question> questions = dao.getQuestions();
+        Assert.assertThat(questions.size(), Is.is(5));
+        questions.forEach(Assert::assertNotNull);
+    }
+}
